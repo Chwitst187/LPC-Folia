f736b788cb6dec9e9c0a0e216f7cb84d8cf49953
Chris <90093334+Chwitst187@users.noreply.github.com>
2026-08-16 13:56:50 +0200

    Update Paper API and chat renderer

--- src/main/java/com/infiniteplugins/lpc/PaperChatListener.java
+++ src/main/java/com/infiniteplugins/lpc/PaperChatListener.java
@@ -1,8 +1,6 @@
 package com.infiniteplugins.lpc;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
-import net.kyori.adventure.text.Component;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,8 +15,7 @@ final class PaperChatListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onChat(final AsyncChatEvent event) {
-		final Player player = event.getPlayer();
-		final Component formatted = plugin.formatPaperMessage(player, event.message());
-		event.renderer((source, sourceDisplayName, message, viewer) -> formatted);
+		event.renderer((source, sourceDisplayName, message, viewer) ->
+				plugin.formatPaperMessage(source, message));
 	}
 }
